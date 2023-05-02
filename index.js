1939530463ede5b48e07a9d3d29bcb9403f3e2b8
MahadiHasanWeb <muhammad.mahadi.hasan.9@gmail.com>
2023-05-03 03:04:37 +0600

    Food Details section added

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ app.get('/', (req, res) =>{
     res.send(chefsData);
 })
 
+app.get('/:id', (req, res) =>{
+    const id = req.params.id;
+    console.log(id);
+    const selectChef = chefsData.find(n => n.id === id);
+    res.send(selectChef)
+})
+
 app.listen(5000, () =>{
     console.log("server is running port 5000")
 })
